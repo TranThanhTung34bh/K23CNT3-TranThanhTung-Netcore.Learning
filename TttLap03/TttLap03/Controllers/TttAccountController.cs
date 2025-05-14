8d2158437efb318fe7594cee071f5f3310040071
unknown <tung345bb@gmail.com>
2025-05-14 08:14:47 +0700

    day3

---
+++ TttLap03/TttLap03/Controllers/TttAccountController.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Principal;
+using Microsoft.AspNetCore.Mvc;
+using TttLap03.Models;
+
+namespace TttLap03.Controllers
+{
+    public class TttAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            var accounts = new List<Account>
+            {
+                new Account { FullName = "Hoàng Anh", Bio = "My name is small", Image = "img1.jpg" },
+                new Account { FullName = "Trường Giang", Bio = "My name is small", Image = "img2.jpg" },
+                new Account { FullName = "Hoàng Thúy", Bio = "My name is small", Image = "img3.jpg" },
+            };
+
+            return View(accounts);
+        }
+
+        public IActionResult Profile()
+        {
+            var account = new Account
+            {
+                FullName = "Hoàng Anh",
+                Email = "anh@gmail.com",
+                Phone = "0986456789",
+                Address = "Hà Nội",
+                Gender = "Male",
+                Birthday = new DateTime(1998, 7, 15),
+                Bio = "My name is small",
+                Image = "img1.jpg"
+            };
+
+            return View(account);
+        }
+    }
+}
---
+++ TttLap03/TttLap03/Controllers/TttHomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TttLap03.Models;
+
+namespace TttLap03.Controllers
+{
+    public class TttHomeController : Controller
+    {
+        private readonly ILogger<TttHomeController> _logger;
+
+        public TttHomeController(ILogger<TttHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult TttIndex()
+        {
+            return View();
+        }
+
+        public IActionResult TttAbout()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
