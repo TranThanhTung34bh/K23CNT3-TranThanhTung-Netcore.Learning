e1945fb4064a518a8dd7405afe5f9a559240fcda
unknown <tung345bb@gmail.com>
2025-05-14 16:13:51 +0700

    day 4

---
+++ Ttt-lab04/Ttt-lab04/Controllers/TttBookcontroller.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ttt_lab04.Models;
+
+namespace Ttt_lab04.Controllers
+{
+    public class TttBookcontroller : Controller
+    {
+
+        private List<TttBook> tttBooks = new List<TttBook>
+{
+    new TttBook
+    {
+        TttId = "1",
+        TttTitle = "Lập trình C# cơ bản",
+        TttDescription = "Hướng dẫn chi tiết lập trình C# từ cơ bản đến nâng cao.",
+        TttImage = "book1.jpg",
+        TttPrice = "500000", // Changed from float to string
+        TttPage = "464", // Fixed: Changed from int to string
+    },
+    new TttBook
+    {
+        TttId = "2",
+        TttTitle = "ASP.NET Core thực chiến",
+        TttDescription = "Xây dựng ứng dụng web hiện đại với ASP.NET Core.",
+        TttImage = "book2.jpg",
+        TttPrice = "450000", // Changed from float to string
+        TttPage = "352", // Fixed: Changed from int to string
+    },
+    new TttBook
+    {
+        TttId = "3",
+        TttTitle = "Cấu trúc dữ liệu & Giải thuật",
+        TttDescription = "Cẩm nang học cấu trúc dữ liệu và giải thuật bằng C#.",
+        TttImage = "algo.jpg",
+        TttPrice = "650000", // Changed from float to string
+        TttPage = "1312", // Fixed: Changed from int to string
+    },
+    new TttBook
+    {
+        TttId = "4",
+        TttTitle = "Thực hành Entity Framework Core",
+        TttDescription = "Hướng dẫn sử dụng EF Core trong các ứng dụng thực tế.",
+        TttImage = "https://m.media-amazon.com/images/I/51k+eUqHjIL._SX258_BO1,204,203,200_.jpg",
+        TttPrice = "520000", // Changed from float to string
+        TttPage = "395", // Fixed: Changed from int to string
+    },
+    new TttBook
+    {
+        TttId = "5",
+        TttTitle = "Refactoring",
+        TttDescription = "Tạo API mạnh mẽ và bảo mật với .NET và JWT.",
+        TttImage = "https://m.media-amazon.com/images/I/41jEbK-jG+L._SX396_BO1,204,203,200_.jpg",
+        TttPrice = "480000", // Changed from float to string
+        TttPage = "448", // Fixed: Changed from int to string
+    }
+};
+
+        public IActionResult TttIndex()
+        {
+            return View(tttBooks); // Fixed typo: Changed "Ttt" to "tttBooks"
+        }
+    }
+}
---
+++ Ttt-lab04/Ttt-lab04/Models/TttBook.cs
@@ -0,0 +1,12 @@
+﻿namespace Ttt_lab04.Models
+{
+    public class TttBook
+    {
+        public string TttId { get; set; }
+        public string TttTitle { get; set; }
+        public string TttDescription { get; set; }
+        public string TttImage { get; set; }
+        public string TttPrice { get; set; }
+        public string TttPage { get; set; }
+    }
+}
