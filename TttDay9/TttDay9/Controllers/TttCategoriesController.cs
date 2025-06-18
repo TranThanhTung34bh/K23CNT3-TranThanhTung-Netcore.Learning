17e0cc9bd0b3c55c4c475373542a98711efd84c2
unknown <tung345bb@gmail.com>
2025-06-18 16:35:35 +0700

    day9

---
+++ TttDay9/TttDay9/Controllers/TttCategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TttDay9.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TttDay9.Controllers
+{
+    public class TttCategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TttDay9/TttDay9/Models/Account.cs
@@ -0,0 +1,6 @@
+﻿namespace TttDay9.Models
+{
+    public class Account
+    {
+    }
+}
